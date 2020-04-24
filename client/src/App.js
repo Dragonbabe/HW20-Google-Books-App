265021531cbc071dbeb3b3726bcfb743d36e56df
Esther Walker <equestriennesrule@yahoo.com>
2020-04-24 15:15:16 -0700

    installed react-router-dom and started nav tabs

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import Search from './components/pages/Search';
 import './App.css';
+import NavBar from './components/pages/NavBar';
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
 
 
 
 
 function App() {
   return (
-  
-    <Search/>
+    <Router>
+      <div className="content">
+        <NavBar/>
+        <Search/>
+        <Switch>
+          <Route exact path="/" component={Search}/>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
